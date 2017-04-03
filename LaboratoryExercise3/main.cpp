0ea319f5aff15f25b84b79c68c44030905d1c732
Marina Rupe <marina.rupe@fer.hr>
2017-04-04 00:47:31 +0200

    Implement lab 4 + refactor lab 3

--- LaboratoryExercise3/main.cpp
+++ LaboratoryExercise3/main.cpp
@@ -18,6 +18,7 @@ class iEdge2D {
         int a;
         int b;
         int c;
+        iEdge2D() : a(0), b(0), c(0) {};
         iEdge2D(int a, int b, int c) : a(a), b(b), c(c) {};
 };
 
@@ -26,7 +27,127 @@ class iPolyElem {
         iPoint2D* vertex;
         iEdge2D* edge;
         bool isLeft;
-        iPolyElem(iPoint2D* vertex, iEdge2D* edge, bool isLeft): vertex(vertex), edge(edge), isLeft(isLeft) {}
+        iPolyElem(iPoint2D* vertex): vertex(vertex), edge(new iEdge2D()), isLeft(false) {}
+};
+
+class iPolygon {
+public:
+    vector<iPolyElem *> polyElems;
+    int n;
+    bool isConvex;
+    int orientation; // 1 -> clockwise
+    iPolygon(vector<iPolyElem *> &polyElems, int n) : polyElems(polyElems), n(n) {
+        calculateEdges();
+        setEdgesOrientation();
+    };
+
+    void calculateEdges() {
+        int i0 = n - 1;
+        for (int i = 0; i < n; i0 = i++) {
+            // dot product of vertices
+            polyElems[i0]->edge->a = polyElems[i0]->vertex->y - polyElems[i]->vertex->y;
+            polyElems[i0]->edge->b = -(polyElems[i0]->vertex->x - polyElems[i]->vertex->x);
+            polyElems[i0]->edge->c = (polyElems[i0]->vertex->x * polyElems[i]->vertex->y
+                                      - polyElems[i0]->vertex->y * polyElems[i]->vertex->x);
+            // is left or right edge
+            polyElems[i0]->isLeft = polyElems[i0]->vertex->y < polyElems[i]->vertex->y;
+        }
+    }
+
+    void setEdgesOrientation() {
+        int dotProduct, up, down, on;
+        down = up = on = 0;
+
+        for (int i = 0, i0 = n - 2; i < n; i++, i0++) {
+            if (i0 >= n) i0 = 0;
+            dotProduct = polyElems[i0]->edge->a * polyElems[i]->vertex->x
+                         + polyElems[i0]->edge->b * polyElems[i]->vertex->y
+                         + polyElems[i0]->edge->c;
+
+            if (dotProduct == 0) on++;
+            else if (dotProduct > 0) up++;
+            else down++;
+        }
+
+        if (down == 0) {
+            isConvex = true;
+            orientation = 0;
+        } else if (up == 0) {
+            isConvex = true;
+            orientation = 1;
+        } else {
+            isConvex = false;
+            orientation = 0;
+        }
+    }
+
+    bool pointInsidePolygon(int x, int y) {
+        int dotProduct;
+        for (int i = 0; i < n; i++) {
+            dotProduct = polyElems[i]->edge->a * x + polyElems[i]->edge->b * y + polyElems[i]->edge->c;
+
+            if (dotProduct > 0) return false;
+        }
+        return true;
+    }
+    void reverseOrientation() {
+        reverse(polyElems.begin(), polyElems.end()); orientation = 1;
+    }
+
+    void draw() {
+        if (isConvex) {
+            for (int i = 0, i0 = n - 1; i < n; i++) {
+                bresenham(polyElems[i0]->vertex->x, polyElems[i0]->vertex->y, polyElems[i]->vertex->x,
+                          polyElems[i]->vertex->y);
+                i0 = i;
+            }
+        }
+    }
+
+    void fill() {
+        int xMin, xMax, yMin, yMax;
+        xMin = xMax = polyElems[0]->vertex->x;
+        yMin = yMax = polyElems[0]->vertex->y;
+
+        for (int i = 1; i < n; i++) {
+            if (xMin > polyElems[i]->vertex->x) xMin = polyElems[i]->vertex->x;
+            if (xMax < polyElems[i]->vertex->x) xMax = polyElems[i]->vertex->x;
+            if (yMin > polyElems[i]->vertex->y) yMin = polyElems[i]->vertex->y;
+            if (yMax < polyElems[i]->vertex->y) yMax = polyElems[i]->vertex->y;
+        }
+
+        int i0;
+        double L, D, x;
+        for (int y = yMin; y <= yMax; y++) {
+            L = xMin;
+            D = xMax;
+            i0 = n - 1;
+
+            for (int i = 0; i < n; i0 = i++) {
+                if (polyElems[i0]->edge->a == 0.) {
+                    if (polyElems[i0]->vertex->y == y) {
+                        if (polyElems[i0]->vertex->x < polyElems[i]->vertex->x) {
+                            L = polyElems[i0]->vertex->x;
+                            D = polyElems[i]->vertex->x;
+                        } else {
+                            L = polyElems[i]->vertex->x;
+                            D = polyElems[i0]->vertex->x;
+                        }
+                        break;
+                    }
+                } else {
+                    x = (-polyElems[i0]->edge->b * y - polyElems[i0]->edge->c) / (double) polyElems[i0]->edge->a;
+
+                    if (polyElems[i0]->isLeft) {
+                        if (L < x) L = x;
+                    } else {
+                        if (D > x) D = x;
+                    }
+                }
+            }
+            bresenham((GLint) round(L), y, (GLint) round(D), y);
+        }
+    }
 };
 
 void myDisplay();
@@ -34,17 +155,9 @@ void myReshape(int width, int height);
 void setPolygon();
 void setPoint();
 void renderScene ();
-void calculateEdges(vector<iPolyElem *> &polyElem, int n);
-void drawConvexPolygon (vector<iPolyElem*> &polyElem, int n);
-void fillConvexPolygon(vector<iPolyElem*> &polyElem, int n);
-void chechEdgesOrientation(vector<iPolyElem *> &polyElem, int n, bool *isConvex, int *orientation);
-bool pointInsidePolygon(int x, int y, vector<iPolyElem *> &polyElem, int n);
 
-vector<iPolyElem*> polyElem;
+iPolygon* polygon;
 bool isPointSetUp = false;
-bool isConvex;
-int orientation; // 1 -> clockwise
-int n = 0;
 
 int main (int argc, char ** argv) {
     //Examples:
@@ -73,17 +186,15 @@ void myDisplay () {
 
 void renderScene () {
     glPointSize (1.0f);
-    if (isConvex && orientation == 1) {
-        if (! isPointSetUp) {
-            cout << "Nacrtaj!" << endl;
-            glColor3f (0.0f, 0.0f, 0.0f) ;
-            drawConvexPolygon (polyElem, n);
-        }
-        else {
-            cout << "Ispuni!" << endl;
-            glColor3f (1.0f, 0.0f, 0.0f);
-            fillConvexPolygon(polyElem, n);
-        }
+    if (! isPointSetUp) {
+        cout << "Nacrtaj!" << endl;
+        glColor3f (0.0f, 0.0f, 0.0f) ;
+        polygon->draw();
+    }
+    else {
+        cout << "Ispuni!" << endl;
+        glColor3f (1.0f, 0.0f, 0.0f);
+        polygon->fill();
     }
 }
 
@@ -97,7 +208,10 @@ void myReshape(int w, int h) {
 }
 
 void setPolygon() {
+    int n = 0;
     string space;
+    vector<iPolyElem*> polyElem;
+
     cout << "Molim broj vrhova poligona? "; cin >> n; getline (cin, space);
     polyElem.resize((unsigned long) n);
 
@@ -109,20 +223,18 @@ void setPolygon() {
         int y = stoi(v.substr((unsigned long) index + 1, v.length()));
 
         iPoint2D* vertex = new iPoint2D(x, y);
-        iEdge2D* edge = new iEdge2D(0, 0, 0);
-        polyElem[i] = new iPolyElem(vertex, edge, true);
+        polyElem[i] = new iPolyElem(vertex);
     }
-    calculateEdges(polyElem, n);
-    chechEdgesOrientation(polyElem, n, &isConvex, &orientation);
+    polygon = new iPolygon(polyElem, n);
 
-    if (!isConvex) {
+    if (! polygon->isConvex) {
         cout << "Poligon je konkavan!" << endl;
         exit(1);
     }
-    if (orientation == 0) {
+    if (polygon->orientation == 0) {
         cout << "Redoslijed vrhova suprotan od smjera kazaljke na satu. Okrećem..." << endl;
-        reverse(polyElem.begin(), polyElem.end()); orientation = 1;
-        calculateEdges(polyElem, n);
+        polygon->reverseOrientation();
+        polygon->calculateEdges();
     }
 }
 
@@ -133,110 +245,8 @@ void setPoint() {
     int x = stoi(point.substr(0, (unsigned long) index));
     int y = stoi(point.substr((unsigned long) index + 1, point.length()));
 
-    bool isPointInsidePOlygon = pointInsidePolygon(x, y, polyElem, n);
+    bool isPointInsidePOlygon = polygon->pointInsidePolygon(x, y);
     if (isPointInsidePOlygon) cout << "TOČKA JE UNUTAR POLIGONA!" << endl;
     else cout << "TOČKA JE IZVAN POLIGONA!" << endl;
     isPointSetUp = true;
-}
-
-bool pointInsidePolygon(int x, int y, vector<iPolyElem *> &polyElem, int n) {
-    int dotProduct;
-    for (int i = 0; i < n; i++) {
-        dotProduct = polyElem[i]->edge->a * x + polyElem[i]->edge->b * y + polyElem[i]->edge->c;
-
-        if (dotProduct > 0) return false;
-    }
-    return true;
-}
-
-void calculateEdges(vector<iPolyElem *> &polyElem, int n) {
-    int i0 = n - 1;
-    for (int i = 0; i < n; i0 = i++) {
-        // dot product of vertices
-        polyElem[i0]->edge->a = polyElem[i0]->vertex->y - polyElem[i]->vertex->y;
-        polyElem[i0]->edge->b = -(polyElem[i0]->vertex->x - polyElem[i]->vertex->x);
-        polyElem[i0]->edge->c = (polyElem[i0]->vertex->x * polyElem[i]->vertex->y
-                                 - polyElem[i0]->vertex->y * polyElem[i]->vertex->x);
-        // is left or right edge
-        polyElem[i0]->isLeft = polyElem[i0]->vertex->y < polyElem[i]->vertex->y;
-    }
-}
-
-void chechEdgesOrientation(vector<iPolyElem *> &polyElem, int n, bool *isConvex, int *orientation) {
-    int dotProduct;
-    int up, down, on;
-    down = up = on = 0;
-
-    for (int i = 0, i0 = n - 2; i < n; i++, i0++) {
-        if (i0 >= n) i0 = 0;
-        dotProduct = polyElem[i0]->edge->a * polyElem[i]->vertex->x
-            + polyElem[i0]->edge->b * polyElem[i]->vertex->y
-            + polyElem[i0]->edge->c;
-
-        if (dotProduct == 0) on++;
-        else if (dotProduct > 0) up++;
-        else down++;
-    }
-
-    if (down == 0) {
-        *isConvex = true; *orientation = 0;
-    }
-    else if (up == 0) {
-        *isConvex = true; *orientation = 1;
-    }
-    else {
-        *isConvex = false; *orientation = 0;
-    }
-}
-
-void drawConvexPolygon (vector<iPolyElem*> &polyElem, int n) {
-    for (int i = 0, i0 = n - 1; i < n; i0 = i, i++) {
-        bresenham(polyElem[i0]->vertex->x, polyElem[i0]->vertex->y, polyElem[i]->vertex->x, polyElem[i]->vertex->y);
-    }
-}
-
-void fillConvexPolygon (vector<iPolyElem*> &polyElem, int n) {
-    int xMin, xMax, yMin, yMax;
-    xMin = xMax = polyElem[0]->vertex->x;
-    yMin = yMax = polyElem[0]->vertex->y;
-
-    for (int i = 1; i < n; i++) {
-        if (xMin > polyElem[i]->vertex->x) xMin = polyElem[i]->vertex->x;
-        if (xMax < polyElem[i]->vertex->x) xMax = polyElem[i]->vertex->x;
-        if (yMin > polyElem[i]->vertex->y) yMin = polyElem[i]->vertex->y;
-        if (yMax < polyElem[i]->vertex->y) yMax = polyElem[i]->vertex->y;
-    }
-
-    int i0; double L, D, x;
-    for (int y = yMin; y <= yMax; y++) {
-        L = xMin; D = xMax;
-        i0 = n - 1;
-
-        for (int i = 0; i < n; i0 = i++) {
-            if (polyElem[i0]->edge->a == 0.) {
-                if (polyElem[i0]->vertex->y == y) {
-                    if (polyElem[i0]->vertex->x < polyElem[i]->vertex->x) {
-                        L = polyElem[i0]->vertex->x;
-                        D = polyElem[i]->vertex->x;
-                    }
-                    else {
-                        L = polyElem[i]->vertex->x;
-                        D = polyElem[i0]->vertex->x;
-                    }
-                    break;
-                }
-            }
-            else {
-                x = (-polyElem[i0]->edge->b * y - polyElem[i0]->edge->c) / (double)polyElem[i0]->edge->a;
-
-                if (polyElem[i0]->isLeft) {
-                    if (L < x) L = x;
-                }
-                else {
-                    if (D > x ) D = x;
-                }
-            }
-        }
-        bresenham((GLint)round(L), y, (GLint)round(D), y);
-    }
 }
--- LaboratoryExercise4/main.cpp
+++ LaboratoryExercise4/main.cpp
@@ -1,6 +1,217 @@
 #include <iostream>
+#include <GL/glut.h>
+#include <vector>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class Point3D {
+public:
+    float x;
+    float y;
+    float z;
+    Point3D(float x, float y, float z) : x(x), y(y), z(z) {};
+};
+
+class PlaneCoefficients {
+public:
+    float A;
+    float B;
+    float C;
+    float D;
+    PlaneCoefficients(float A, float B, float C, float D) : A(A), B(B), C(C), D(D) {};
+    PlaneCoefficients() : A(0), B(0), C(0), D(0) {};
+};
+
+class Polygon {
+public:
+    vector<Point3D*> vertices;
+    PlaneCoefficients* coefficients;
+    Polygon(vector<Point3D*> &vertices) : vertices(vertices), coefficients(new PlaneCoefficients()) {};
+    void calculatePlaneCoefficients() {
+        coefficients->A = (vertices[1]->y - vertices[0]->y) * (vertices[2]->z - vertices[0]->z)
+                          - (vertices[1]->z - vertices[0]->z) * (vertices[2]->y - vertices[0]->y);
+
+        coefficients->B = -(vertices[1]->x - vertices[0]->x) * (vertices[2]->z - vertices[0]->z)
+                          + (vertices[1]->z - vertices[0]->z) * (vertices[2]->x - vertices[0]->x);
+
+        coefficients->C = (vertices[1]->x - vertices[0]->x) * (vertices[2]->y - vertices[0]->y)
+                          - (vertices[1]->y - vertices[0]->y) * (vertices[2]->x - vertices[0]->x);
+
+        coefficients->D = -vertices[0]->x * coefficients->A -vertices[0]->y * coefficients->B - vertices[0]->z * coefficients->C;
+    }
+    bool containsPoint(Point3D *testPoint) {
+        double dotProduct = coefficients->A * testPoint->x
+                     + coefficients->B * testPoint->y
+                     + coefficients->C * testPoint->z
+                     + coefficients->D;
+
+        return (dotProduct <= 0.0);
+    }
+    void draw() {
+        int n = (int)vertices.size();
+        for (int i = 0, i0 = n - 1; i < n; i++) {
+            glBegin(GL_LINES);
+            glVertex3f(vertices[i0]->x, vertices[i0]->y, vertices[i0]->z);
+            glVertex3f(vertices[i]->x, vertices[i]->y, vertices[i]->z);
+            glEnd();
+            i0 = i;
+        }
+    }
+};
+
+class Figure {
+public:
+    vector<Polygon*> polygons;
+    Figure(vector<Polygon*> &polygons) : polygons(polygons) {
+        calculatePlaneCoefficients();
+    };
+    bool isPointInsideFigure(Point3D *testPoint) {
+        for (int i = 0; i < polygons.size(); i++) {
+            if (! (polygons[i]->containsPoint(testPoint))) return false;
+        }
+        return true;
+    }
+    void draw() {
+        for (int i = 0; i < polygons.size(); i++) {
+            polygons[i]->draw();
+        }
+    }
+private:
+    void calculatePlaneCoefficients() {
+        for (int i = 0; i < polygons.size(); i++) {
+            polygons[i]->calculatePlaneCoefficients();
+        }
+    }
+};
+
+Figure* figure;
+string DIR = "/home/objects/";
+float xMax, yMax, zMax;
+float xMin, yMin, zMin;
+
+void myDisplay();
+void renderScene();
+void myReshape(int w, int h);
+void loadObject();
+Point3D* setPoint();
+
+int main(int argc, char ** argv) {
+    loadObject();
+    Point3D* testPoint = setPoint();
+    cout << "Točka je " << (figure->isPointInsideFigure(testPoint) ? "unutar" : "izvan") << " tijela." << endl;
+    glutInit(&argc, argv) ;
+    glutInitDisplayMode(GLUT_DOUBLE ) ;
+    glutInitWindowSize(700, 700) ;
+    glutInitWindowPosition(100, 100) ;
+    glutCreateWindow("Polygon") ;
+    glutDisplayFunc(myDisplay) ;
+    glutReshapeFunc(myReshape) ;
+    glutMainLoop() ;
     return 0;
+}
+
+void myDisplay() {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT) ;
+    glLoadIdentity();
+    renderScene();
+    glutSwapBuffers();
+}
+
+void renderScene() {
+    glPointSize(1.5f);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    //glRotatef(45.0, 0.0, 1.0, 0.0);
+    figure->draw();
+}
+
+void myReshape(int w, int h) {
+    glDisable(GL_DEPTH_TEST);
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode (GL_PROJECTION) ;
+    glLoadIdentity();
+
+    float min = xMin, max = xMax;
+    if (yMin < min) min = yMin; if (zMin < min) min = zMin;
+    if (yMax > max) max = yMax; if (zMax > max) max = zMax;
+    if (min == 0) min = -(max/2);
+
+    glOrtho(1.5f * min, 1.5f * max, 1.5f * min, 1.5f * max, 1.5f * min, 1.5f * max);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void loadObject() {
+    string object;
+    cout << "Odaberite tijelo: " << endl; getline (cin, object);
+    ifstream file(DIR + object + ".obj");
+
+    if(!file) {
+        cout << "Cannot open input file.\n";
+        exit(1);
+    }
+    vector<Point3D*> vertices;
+    vector<Polygon*> polygons;
+    string line;
+    int n = 0;
+    while (getline(file, line)) {
+        char startsWith = line[0];
+
+        if (startsWith == '#') continue;
+
+        else if (startsWith == 'v') {
+            int len = (int) line.length();
+            vector<float> point;
+            n++;
+            for (int i = 0; i < 4; i++) {
+                int index = (int) line.find(" ");
+
+                if (i != 0) {  // skip starting sign ('v')
+                    float p = stof(line.substr(0, (unsigned long) index));
+                    point.push_back(p);
+                }
+                line = line.substr((unsigned long) (index + 1), (unsigned long) len);
+            }
+            vertices.push_back(new Point3D(point[0], point[1], point[2]));
+
+            if (point[0] > xMax) xMax = point[0]; if (point[0] < xMin) xMin = point[0];
+            if (point[1] > yMax) yMax = point[1]; if (point[1] < yMin) yMin = point[1];
+            if (point[2] > zMax) zMax = point[2]; if (point[2] < zMin) zMin = point[2];
+            point.clear();
+        }
+        else if (startsWith == 'f') {
+            int len = (int) line.length();
+            vector<Point3D*> polygonVertices;
+
+            for (int i = 0; i < 4; i++) {
+                int index = (int) line.find(" ");
+
+                if (i != 0) {  // skip starting sign ('f')
+                    int vertexIndex = stoi(line.substr(0, (unsigned long) index)) - 1;
+                    polygonVertices.push_back(vertices[vertexIndex]);
+                }
+                line = line.substr((unsigned long) (index + 1), (unsigned long) len);
+            }
+            polygons.push_back(new Polygon(polygonVertices));
+            polygonVertices.clear();
+        }
+    }
+    file.close();
+    figure = new Figure(polygons);
+    vertices.clear(); polygons.clear();
+}
+
+Point3D* setPoint() {
+    const int N = 3;
+    string pointStr;
+    cout << "Molim koordinate točke?" << endl; getline (cin, pointStr);
+    int len = (int) pointStr.length();
+
+    vector<float> point;
+    for (int i = 0; i < N; i++) {
+        int index = (int) pointStr.find(" ");
+        point.push_back(stoi(pointStr.substr(0, (unsigned long) index)));
+        pointStr = pointStr.substr((unsigned long) (index + 1), (unsigned long) len);
+    }
+    return new Point3D(point[0], point[1], point[2]);
 }
